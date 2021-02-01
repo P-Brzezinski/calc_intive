665a687e856de67083a1407da23c6fe32da1cfe9
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2021-02-01 15:59:32 +0100

    Refactor getAction() in MainMenu.class

--- src/main/java/input/Input.java
+++ src/main/java/input/Input.java
@@ -28,10 +28,28 @@ public final class Input {
     public int getInt(String askForInt) {
         System.out.println(askForInt);
         while (!scanner.hasNextInt()) {
-            System.out.println("Number is required input");
+            System.out.println("Number is required input.");
             System.out.println(askForInt);
             scanner.nextLine(); //clear bad input data from keyboard
         }
         return scanner.nextInt();
+    }
+
+    public int getInt(String sPrompt, int nLow, int nHigh) {
+        int nInput;
+        do {
+            System.out.printf("%s (%d-%d): ", sPrompt, nLow, nHigh);
+            while (!scanner.hasNextInt()) {
+                System.out.println("Number is required input.");
+                System.out.printf("%s (%d-%d): ", sPrompt, nLow, nHigh);
+                scanner.nextLine(); //clear bad input data from keyboard
+            }
+            nInput = scanner.nextInt();
+            if (nInput >= nLow && nInput <= nHigh)
+                break;
+            System.out.println("Value out of range.");
+            scanner.nextLine(); //clear bad input data from keyboard
+        } while (true);
+        return nInput;
     }
 }
--- src/main/java/mainMenu/MainMenu.java
+++ src/main/java/mainMenu/MainMenu.java
@@ -16,32 +16,25 @@ public class MainMenu {
     public void init() {
         Value valueFromString;
         do {
-            value1 = input.getString("Enter first value:");
-            valueFromString = getValueFromString(value1);
-        } while (valueFromString.equals(UNRECOGNIZED));
+            do {
+                value1 = input.getString("Enter first value:");
+                valueFromString = getValueFromString(value1);
+            } while (valueFromString.equals(UNRECOGNIZED));
 
-        showPossibleCalcs(valueFromString);
-        calc = getAction(valueFromString);
+            showPossibleCalcs(valueFromString);
+            calc = getAction(valueFromString);
 
-        do {
-            value2 = input.getString("Enter second value:");
-            valueFromString = getValueFromString(value2);
-        } while (valueFromString.equals(UNRECOGNIZED));
+            do {
+                value2 = input.getString("Enter second value:");
+                valueFromString = getValueFromString(value2);
+            } while (valueFromString.equals(UNRECOGNIZED));
 
-        System.out.println(value1 + ", " + value2 + ", " + calc.getDescription());
+            System.out.println(value1 + ", " + value2 + ", " + calc.getDescription());
+        } while (true);
     }
 
     private Calc getAction(Value value) {
-        boolean flag = true;
-        int pickOption = 0;
-        while (flag){
-             pickOption = input.getInt("Choose calculation:");
-             if (pickOption <= value.getPossibleCalcs().length){
-                 flag = false;
-             }else {
-                 System.out.println("Wrong calculation number, try again.");
-             }
-        }
+        int pickOption = input.getInt("Choose calculation", 1, value.getPossibleCalcs().length);
         return value.getPossibleCalcs()[pickOption - 1];
     }
 }
